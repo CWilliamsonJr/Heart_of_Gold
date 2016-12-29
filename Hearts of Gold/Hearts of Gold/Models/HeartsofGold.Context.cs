b9f8f585afa563a93ef690f3cb7d2e97968e0ac9
djbw200 <S01088135@acad.tri-c.edu>
2016-12-29 11:22:51 -0500

    fixed errors

--- Hearts of Gold/Hearts of Gold/Models/HeartsofGold.Context.cs
+++ Hearts of Gold/Hearts of Gold/Models/HeartsofGold.Context.cs
@@ -11,11 +11,9 @@ namespace Hearts_of_Gold.Models
 {
     using System;
     using System.Data.Entity;
+    using System.Data.Entity.Core.Objects;
     using System.Data.Entity.Infrastructure;
-    using System.Data.Objects;
-    using System.Data.Objects.DataClasses;
-    using System.Linq;
-    
+
     public partial class Hearts_of_GoldEntities1 : DbContext
     {
         public Hearts_of_GoldEntities1()

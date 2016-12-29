e7346dbb0c0520647330fe524e9e3b61ac5e5346
djbw200 <S01088135@acad.tri-c.edu>
2016-12-29 09:07:27 -0500

    Adding MVC5 Identity

---
+++ Hearts of Gold/Hearts of Gold/App_Start/DBContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Hearts_of_Gold.App_Start
+{
+    public class MyDbContext : IdentityDbContext<AppUser>
+    {
+    }
+}

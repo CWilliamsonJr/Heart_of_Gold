168c8c01adebab4c985b4df11244789680c4fbd1
Dana Anderson <dananderson9591@gmail.com>
2017-01-05 09:57:53 -0500

    Change Contact Page

--- Hearts of Gold/Hearts of Gold/Controllers/HomeController.cs
+++ Hearts of Gold/Hearts of Gold/Controllers/HomeController.cs
@@ -15,14 +15,14 @@ namespace Hearts_of_Gold.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Your application description page" ;
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Our contact page.";
 
             return View();
         }
